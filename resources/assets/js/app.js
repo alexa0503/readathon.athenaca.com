c88066a9fb1e3ed7ead58b1e97340f69a60d1bb9
alexa0503 <lori.w@live.cn>
2018-06-01 11:46:28 +0800

    ga 用户注册情况检测

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -77,16 +77,15 @@ router.beforeEach((to, from, next) => {
         document.body.style.background = '#7fe2bf';
         //document.body.style.background = "#7fe2bf url('/images/bkg-01.png') 0 0 no-repeat";
     }
+    var registerStatus = store.state.self.name != null ? '已注册' : '未注册';
+    
     gtag('config', 'UA-117289831-2', {
         'page_title' : to.name,
-        'page_path': to.fullPath
+        'page_path': to.fullPath,
+        'user_id': store.state.self.id,
+        'custom_map': {'dimension2':'register_status'}
     });
-    var registerStatus = store.state.self.name != null ? '已注册' : '未注册';
-    gtag('event', 'user_view', {
-        'event_category': 'view',
-        'event_label': registerStatus,
-        'value': store.state.self.id
-    })
+    gtag('event', 'register_status_dimension', {'register_status': registerStatus});
     next()
 })
 router.afterEach((to, from)=>{
