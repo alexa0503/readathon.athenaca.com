34727bb32c9fe832d9e5fff649f611e91c401f7a
alexa0503 <lori.w@live.cn>
2018-07-10 14:00:10 +0800

    分享

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -37,9 +37,8 @@ let wxShare = async function (to) {
     }
     // IOS只需要调用一次config
     let u = window.navigator.userAgent
-    console.log(u)
     if( !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) ){
-        need_share_init_config = false
+        //need_share_init_config = false
     }
     var share_desc, shareTimelineDesc
     if (store.state.self.has_joined == 1) {
